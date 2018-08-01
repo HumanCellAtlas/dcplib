23499a9f55feefcb0542eaed07a9d072cb983ef4
Matt Weiden <538456+mweiden@users.noreply.github.com>
2018-08-01 09:25:34 -0700

    Config multipart chunk size in s3 etag and checksumming buffered reader

--- tests/checksumming_io/test_checksumming_buffered_reader.py
+++ tests/checksumming_io/test_checksumming_buffered_reader.py
@@ -7,11 +7,15 @@ import io, os, sys, unittest
 pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))  # noqa
 sys.path.insert(0, pkg_root)  # noqa
 
+from dcplib import s3_multipart
 from dcplib.checksumming_io import ChecksummingBufferedReader
 from . import TEST_FILE, TEST_FILE_CHECKSUMS
 
 
 class TestChecksummingBufferedReader(unittest.TestCase):
+
+    file_size = os.path.getsize(TEST_FILE)
+    chunk_size = s3_multipart.get_s3_multipart_chunk_size(file_size)
 
     def check_sums(self, checksums):
         self.assertEqual(checksums['sha1'], TEST_FILE_CHECKSUMS['sha1'])
@@ -21,18 +25,16 @@ class TestChecksummingBufferedReader(unittest.TestCase):
 
     def test_checksums_after_single_read(self):
         with io.open(TEST_FILE, 'rb') as fh:
-            reader = ChecksummingBufferedReader(fh)
+            reader = ChecksummingBufferedReader(fh, self.chunk_size)
             reader.read()
             sums = reader.get_checksums()
             self.check_sums(sums)
 
     def test_checksums_after_multiple_reads(self):
-        statinfo = os.stat(TEST_FILE)
-        chunk_size = statinfo.st_size // 4
         with io.open(TEST_FILE, 'rb') as raw_fh:
-            reader = ChecksummingBufferedReader(raw_fh)
+            reader = ChecksummingBufferedReader(raw_fh, self.chunk_size)
             while True:
-                buf = reader.read(chunk_size)
+                buf = reader.read(self.chunk_size)
                 if not buf:
                     break
             sums = reader.get_checksums()
--- tests/checksumming_io/test_checksumming_sink.py
+++ tests/checksumming_io/test_checksumming_sink.py
@@ -7,11 +7,15 @@ import os, sys, unittest
 pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))  # noqa
 sys.path.insert(0, pkg_root)  # noqa
 
+from dcplib import s3_multipart
 from dcplib.checksumming_io import ChecksummingSink
 from . import TEST_FILE, TEST_FILE_CHECKSUMS
 
 
 class TestChecksummingSink(unittest.TestCase):
+
+    file_size = os.path.getsize(TEST_FILE)
+    chunk_size = s3_multipart.get_s3_multipart_chunk_size(file_size)
 
     def check_sums(self, checksums):
         self.assertEqual(checksums['sha1'], TEST_FILE_CHECKSUMS['sha1'])
@@ -20,7 +24,7 @@ class TestChecksummingSink(unittest.TestCase):
         self.assertEqual(checksums['s3_etag'], TEST_FILE_CHECKSUMS['s3_etag'])
 
     def test_checksums_after_single_write(self):
-        sink = ChecksummingSink()
+        sink = ChecksummingSink(self.chunk_size)
         with open(TEST_FILE, 'rb') as fh:
             data = fh.read()
             sink.write(data)
@@ -28,12 +32,10 @@ class TestChecksummingSink(unittest.TestCase):
         self.check_sums(sums)
 
     def test_checksums_after_multiple_write(self):
-        sink = ChecksummingSink()
-        statinfo = os.stat(TEST_FILE)
-        chunk_size = statinfo.st_size // 4
+        sink = ChecksummingSink(self.chunk_size)
         with open(TEST_FILE, 'rb') as fh:
             while True:
-                data = fh.read(chunk_size)
+                data = fh.read(self.chunk_size)
                 if not data:
                     break
                 sink.write(data)
@@ -41,7 +43,7 @@ class TestChecksummingSink(unittest.TestCase):
         self.check_sums(sums)
 
     def test_hash_function_list_is_configurable(self):
-        sink = ChecksummingSink(hash_functions=('sha1', 's3_etag'))
+        sink = ChecksummingSink(self.chunk_size, hash_functions=('sha1', 's3_etag'))
         with open(TEST_FILE, 'rb') as fh:
             data = fh.read()
             sink.write(data)
@@ -50,7 +52,7 @@ class TestChecksummingSink(unittest.TestCase):
         self.assertEqual(sums['sha1'], TEST_FILE_CHECKSUMS['sha1'])
         self.assertEqual(sums['s3_etag'], TEST_FILE_CHECKSUMS['s3_etag'])
 
-        sink = ChecksummingSink(hash_functions=('sha256', 'crc32c'))
+        sink = ChecksummingSink(self.chunk_size, hash_functions=('sha256', 'crc32c'))
         with open(TEST_FILE, 'rb') as fh:
             data = fh.read()
             sink.write(data)
