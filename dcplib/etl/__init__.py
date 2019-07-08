f1d78385c65f70d4e03dfebe5dec6da1b9fa8b95
Madison Dunitz <madison.dunitz@chanzuckerberg.com>
2019-07-08 15:10:41 -0700

    refactor dss extractor (#28)
    * refactor dssextractor
    
    * refactor extractor
    
    * revert to original function signature, paginate results
    
    * reset temp directory before each test
    
    * use context manager to handle tempdirectory
    
    * skip test_etl_with_dispatcher

--- dcplib/etl/__init__.py
+++ dcplib/etl/__init__.py
@@ -1,35 +1,30 @@
 """
 Shared ETL (extract, transform, load) code for fetching and loading data from HCA DSS.
-
 The main entry point is the DSSExtractor.extract() function, which takes optional transform and load callbacks that
 will be invoked for each bundle fetched.
-
+Performance gains come from amortizing latency for network calls to retrieve and if necessary checkout a bundle.
+Caching on the file system reduces the need to retrieve files and interleaving the extract, transform and load steps
+makes testing transform and load functions faster.
 Example usage:
-
     from dcplib.etl import DSSExtractor
-
     def tf(*args, **kwargs):
         print("Transformer", args, kwargs)
         return "TEST"
-
     def ld(*args, **kwargs):
         print("Loader", args, kwargs)
-
     def fn(*args, **kwargs):
         print("Finalizer", args, kwargs)
-
     DSSExtractor(staging_directory=".").extract(transformer=tf, loader=ld, finalizer=fn)
 """
 
-import os, sys, json, concurrent.futures, hashlib, logging, threading
-from collections import defaultdict
+import os, sys, json, concurrent.futures, hashlib, logging, threading, time
 from fnmatch import fnmatchcase
 
 import hca
 from ..networking import http
-from hca.util import RetryPolicy
 
 logger = logging.getLogger(__name__)
+
 
 class DSSExtractor:
     default_bundle_query = {'query': {'bool': {'must_not': {'term': {'admin_deleted': True}}}}}
@@ -40,8 +35,6 @@ class DSSExtractor:
         self.sd = staging_directory
         self.content_type_patterns = content_type_patterns or self.default_content_type_patterns
         self.filename_patterns = filename_patterns or []
-        self.b2f = defaultdict(set)
-        self.staged_bundles = []
         self._dss_client = dss_client
         self._dss_swagger_url = None
         self._dispatch_on_empty_bundles = dispatch_on_empty_bundles
@@ -60,33 +53,55 @@ class DSSExtractor:
             self._dss_client = hca.dss.DSSClient(swagger_url=self._dss_swagger_url)
         return self._dss_client
 
-    def link_file(self, bundle_uuid, bundle_version, f):
-        if not os.path.exists(f"{self.sd}/bundles/{bundle_uuid}.{bundle_version}/{f['name']}"):
-            logger.debug("Linking fetched file %s/%s", bundle_uuid, f["name"])
-            os.symlink(f"../../files/{f['uuid']}.{f['version']}",
-                       f"{self.sd}/bundles/{bundle_uuid}.{bundle_version}/{f['name']}")
+    def extract_one(self, bundle_uuid, bundle_version, transformer: callable = None):
+        bundle_uuid, bundle_version, fetched_files = self.get_files_to_fetch_for_bundle(bundle_uuid, bundle_version)
+        bundle_path = f"{self.sd}/bundles/{bundle_uuid}.{bundle_version}"
+        bundle_manifest_path = f"{self.sd}/bundle_manifests/{bundle_uuid}.{bundle_version}.json"
+        if not os.path.exists(bundle_path):
+            if self._dispatch_on_empty_bundles:
+                bundle_path = None
+            else:
+                return
+        if transformer is not None:
+            tb = transformer(bundle_uuid=bundle_uuid, bundle_version=bundle_version, bundle_path=bundle_path,
+                             bundle_manifest_path=bundle_manifest_path, extractor=self)
 
-    def get_file(self, f, bundle_uuid, bundle_version, print_progress=True):
-        logger.debug("[%s] Fetching %s:%s", threading.current_thread().getName(), bundle_uuid, f["name"])
-        res = http.get(f"{self.dss_client.host}/files/{f['uuid']}", params={"replica": "aws", "version": f["version"]})
-        res.raise_for_status()
-        with open(f"{self.sd}/files/{f['uuid']}.{f['version']}", "wb") as fh:
-            fh.write(res.content)
-        self.link_file(bundle_uuid, bundle_version, f)
-        logger.debug("Wrote %s:%s", bundle_uuid, f["name"])
-        if print_progress:
-            sys.stdout.write(".")
-            sys.stdout.flush()
-        return f, bundle_uuid, bundle_version
+            return tb
 
-    def should_fetch_file(self, f):
-        if any(fnmatchcase(f["content-type"], p) for p in self.content_type_patterns):
-            return True
-        if any(fnmatchcase(f["name"], p) for p in self.filename_patterns):
-            return True
-        return False
+    def extract(self, query, max_workers=512, max_dispatchers=1,
+                dispatch_executor_class: concurrent.futures.Executor = concurrent.futures.ThreadPoolExecutor,
+                transformer: callable = None, loader: callable = None, finalizer: callable = None):
+        start = time.time()
+        if query is None:
+            query = self.default_bundle_query
+        total_bundles = self.dss_client.post_search(es_query=query, replica="aws")["total_hits"]
+        if total_bundles == 0:
+            logger.error("No bundles found, nothing to do")
+            return
+        logger.info("Scanning %s bundles", total_bundles)
+        loaded_bundle_count = 0
+        with dispatch_executor_class(max_workers=max_workers) as executor:
+            for page in self.dss_client.post_search.paginate(es_query=query, replica="aws", per_page=500):
+                if loaded_bundle_count % 100 == 0:
+                    logger.info(f"\nLoading bundles: {loaded_bundle_count} loaded so far. "
+                                f"{round(100*(loaded_bundle_count/total_bundles))}% of bundles loaded")
+                futures = []
+                for bundle in page['results']:
+                    bundle_uuid, bundle_version = bundle["bundle_fqid"].split(".", 1)
+                    futures.append(executor.submit(self.extract_one, bundle_uuid, bundle_version, transformer))
+
+                if loader is not None:
+                    for future in concurrent.futures.as_completed(futures):
+                        loader(bundle=future.result())
+                        loaded_bundle_count += 1
+
+        if finalizer is not None:
+            finalizer(extractor=self)
+        end = time.time()
+        logger.info(f"Loaded {loaded_bundle_count} bundles in {round(end - start)} seconds!")
 
     def get_files_to_fetch_for_bundle(self, bundle_uuid, bundle_version):
+        # get the bundle manifest and store in file system
         try:
             with open(f"{self.sd}/bundle_manifests/{bundle_uuid}.{bundle_version}.json") as fh:
                 bundle_manifest = json.load(fh)
@@ -101,98 +116,48 @@ class DSSExtractor:
                 json.dump(bundle_manifest, fh)
 
         logger.debug("Scanning bundle %s", bundle_uuid)
-        files_to_fetch = []
+        fetched_files = []
+        # For each file in the bundle, check if it has been fetched, validate the checksum, and link it in the bundle
+        # directory. Otherwise, call _get_file() to fetch it.
         for f in bundle_manifest["files"]:
-            if self.should_fetch_file(f):
+            if self._should_fetch_file(f):
                 os.makedirs(f"{self.sd}/bundles/{bundle_uuid}.{bundle_version}", exist_ok=True)
+                os.makedirs(f"{self.sd}/files/{bundle_uuid}.{bundle_version}", exist_ok=True)
                 try:
                     with open(f"{self.sd}/files/{f['uuid']}.{f['version']}", "rb") as fh:
                         file_csum = hashlib.sha256(fh.read()).hexdigest()
                         if file_csum == f["sha256"]:
-                            self.link_file(bundle_uuid, bundle_version, f)
+                            self._link_file(bundle_uuid, bundle_version, f)
                             continue
-                except (FileNotFoundError, ):
-                    pass
-                files_to_fetch.append(f)
+                except (FileNotFoundError,):
+                    self._get_file(f, bundle_uuid, bundle_version)
+                    fetched_files.append(f)
             else:
                 logger.debug("Skipping file %s/%s (no filter match)", bundle_uuid, f["name"])
-        return bundle_uuid, bundle_version, files_to_fetch
+        return bundle_uuid, bundle_version, fetched_files
 
-    def enqueue_bundle_manifests(self, percent_complete, bundles_complete, total_bundles, f2f_futures, ff_futures,
-                                 executor):
-        logger.info("[%d%%] [%d/%d] Processing bundle batch",
-                    percent_complete, bundles_complete, total_bundles)
-        for future in concurrent.futures.as_completed(f2f_futures):
-            bundle_uuid, bundle_version, files_to_fetch = future.result()
-            if files_to_fetch:
-                logger.info("[%d%%] Fetching %d files for bundle %s @ %s",
-                            percent_complete, len(files_to_fetch), bundle_uuid, bundle_version)
-                for f in files_to_fetch:
-                    ff_futures.append(executor.submit(self.get_file, f, bundle_uuid, bundle_version))
-                    self.b2f[bundle_uuid + "." + bundle_version].add(f["name"])
-            else:
-                self.staged_bundles.append([bundle_uuid, bundle_version])
-        return len(f2f_futures)
+    def _should_fetch_file(self, f):
+        if any(fnmatchcase(f["content-type"], p) for p in self.content_type_patterns):
+            return True
+        if any(fnmatchcase(f["name"], p) for p in self.filename_patterns):
+            return True
+        return False
 
-    def extract(self, query: dict = None, transformer: callable = None, loader: callable = None,
-                finalizer: callable = None, max_workers=512, max_dispatchers=1,
-                dispatch_executor_class: concurrent.futures.Executor = concurrent.futures.ThreadPoolExecutor):
-        if query is None:
-            query = self.default_bundle_query
-        os.makedirs(f"{self.sd}/files", exist_ok=True)
-        os.makedirs(f"{self.sd}/bundles", exist_ok=True)
-        f2f_futures, ff_futures = [], []
-        bundles_complete, percent_complete = 0, -1
-        with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
-            total_bundles = self.dss_client.post_search(es_query=query, replica="aws")["total_hits"]
-            if total_bundles == 0:
-                logger.error("No bundles found, nothing to do")
-                return
-            logger.info("Scanning %s bundles", total_bundles)
-            for b in self.dss_client.post_search.iterate(es_query=query, replica="aws", per_page=500):
-                bundle_uuid, bundle_version = b["bundle_fqid"].split(".", 1)
-                f2f_futures.append(executor.submit(self.get_files_to_fetch_for_bundle, bundle_uuid, bundle_version))
-                if len(f2f_futures) > max_workers * 2:
-                    bundles_complete += self.enqueue_bundle_manifests(percent_complete, bundles_complete, total_bundles,
-                                                                      f2f_futures, ff_futures, executor)
-                    f2f_futures.clear()
+    def _link_file(self, bundle_uuid, bundle_version, f):
+        if not os.path.exists(f"{self.sd}/bundles/{bundle_uuid}.{bundle_version}/{f['name']}"):
+            logger.debug("Linking fetched file %s/%s", bundle_uuid, f["name"])
+            os.symlink(f"../../files/{f['uuid']}.{f['version']}",
+                       f"{self.sd}/bundles/{bundle_uuid}.{bundle_version}/{f['name']}")
 
-                if int((bundles_complete / total_bundles) * 100) > percent_complete:
-                    percent_complete = int((bundles_complete / total_bundles) * 100)
-                    logger.info("%d%% complete (%d/%d bundles)", percent_complete, bundles_complete, total_bundles)
-            bundles_complete += self.enqueue_bundle_manifests(percent_complete, bundles_complete, total_bundles,
-                                                              f2f_futures, ff_futures, executor)
-
-        with dispatch_executor_class(max_workers=max_dispatchers) as executor:
-            dispatch_callbacks_futures = []
-            for future in concurrent.futures.as_completed(ff_futures):
-                f, bundle_uuid, bundle_version = future.result()
-                self.b2f[bundle_uuid + "." + bundle_version].remove(f["name"])
-                if len(self.b2f[bundle_uuid + "." + bundle_version]) == 0:
-                    future = executor.submit(self.dispatch_callbacks, bundle_uuid, bundle_version, transformer, loader)
-                    dispatch_callbacks_futures.append(future)
-                else:
-                    logger.debug("%s: %d files to go", bundle_uuid, len(self.b2f[bundle_uuid + "." + bundle_version]))
-            for bundle_uuid, bundle_version in self.staged_bundles:
-                future = executor.submit(self.dispatch_callbacks, bundle_uuid, bundle_version, transformer, loader)
-                dispatch_callbacks_futures.append(future)
-            for future in concurrent.futures.as_completed(dispatch_callbacks_futures):
-                future.result()
-
-        if finalizer is not None:
-            finalizer(extractor=self)
-        logger.info("Done (%d/%d bundles)", bundles_complete, total_bundles)
-
-    def dispatch_callbacks(self, bundle_uuid, bundle_version, transformer, loader):
-        bundle_path = f"{self.sd}/bundles/{bundle_uuid}.{bundle_version}"
-        bundle_manifest_path = f"{self.sd}/bundle_manifests/{bundle_uuid}.{bundle_version}.json"
-        if not os.path.exists(bundle_path):
-            if self._dispatch_on_empty_bundles:
-                bundle_path = None
-            else:
-                return
-        if transformer is not None:
-            tb = transformer(bundle_uuid=bundle_uuid, bundle_version=bundle_version, bundle_path=bundle_path,
-                             bundle_manifest_path=bundle_manifest_path, extractor=self)
-        if loader is not None:
-            loader(extractor=self, transformer=transformer, bundle=tb)
+    def _get_file(self, f, bundle_uuid, bundle_version, print_progress=True):
+        logger.debug("[%s] Fetching %s:%s", threading.current_thread().getName(), bundle_uuid, f["name"])
+        res = http.get(f"{self.dss_client.host}/files/{f['uuid']}", params={"replica": "aws", "version": f["version"]})
+        res.raise_for_status()
+        with open(f"{self.sd}/files/{f['uuid']}.{f['version']}", "wb") as fh:
+            fh.write(res.content)
+        self._link_file(bundle_uuid, bundle_version, f)
+        logger.debug("Wrote %s:%s", bundle_uuid, f["name"])
+        if print_progress:
+            sys.stdout.write(".")
+            sys.stdout.flush()
+        return f, bundle_uuid, bundle_version
--- tests/test_etl.py
+++ tests/test_etl.py
@@ -1,4 +1,5 @@
-import unittest, io, sys, json, tempfile, logging, concurrent.futures
+import tempfile
+import unittest, io, sys, json, logging, concurrent.futures
 from collections import defaultdict
 from requests.models import Response
 
@@ -6,114 +7,159 @@ logger = logging.getLogger(__name__)
 
 calls = defaultdict(int)
 
+
 def tf(*args, **kwargs):
     logger.debug("Transformer called with %s %s", args, kwargs)
     calls["tf"] += 1
     return "TEST"
+
 
 def ld(*args, **kwargs):
     logger.debug("Loader called with %s %s", args, kwargs)
     calls["ld"] += 1
     assert kwargs["bundle"] == "TEST"
 
+
 def fn(*args, **kwargs):
     logger.debug("Finalizer called with %s %s", args, kwargs)
     calls["fn"] += 1
 
+
 class TestETLException(Exception):
     pass
+
 
 def error_fn(*args, **kwargs):
     logger.debug("Finalizer called with %s %s, raising error", args, kwargs)
     raise TestETLException("test")
 
+
+files = [
+    {"name": hex(i), "content-type": "application/json", "uuid": hex(i), "version": "1", "sha256": "0"}
+    for i in range(256)
+]
+
+
+class MockDSSClient:
+    host = "localhost"
+    swagger_url = "swagger_url"
+
+    class MockDSSMethod:
+        def __call__(self, es_query, replica):
+            return {"total_hits": 1}
+
+        def iterate(self, es_query, replica, per_page):
+            for i in range(4):
+                yield {"bundle_fqid": "a%d.b" % i}
+
+        def paginate(self, es_query, replica, per_page):
+            for i in range(4):
+                yield {"results": [{"bundle_fqid": "a%d.b" % i}]}
+
+    post_search = MockDSSMethod()
+
+    def get(self, url, params):
+        if "files" in url:
+            payload = {}
+        else:
+            payload = {"bundle": {"files": files}}
+        res = Response()
+        res.status_code = 200
+        res.raw = io.BytesIO(json.dumps(payload).encode())
+        return res
+
+
 class TestETL(unittest.TestCase):
+
+    def setUp(self):
+        import dcplib.etl
+        dcplib.etl.http = MockDSSClient()
+
+        calls["tf"] = 0
+        calls["fn"] = 0
+        calls["ld"] = 0
+
     @unittest.skipIf(sys.version_info < (3, 6), "Only testing under Python 3.6+")
     def test_etl(self):
-        from unittest.mock import MagicMock, patch
         import dcplib.etl
-
-        files = [
-            {"name": hex(i), "content-type": "application/json", "uuid": hex(i), "version": "1", "sha256": "0"}
-            for i in range(256)
-        ]
-
-        class MockDSSClient:
-            host = "localhost"
-            swagger_url = "swagger_url"
-
-            class MockDSSMethod:
-                def __call__(self, es_query, replica):
-                    return {"total_hits": 1}
-
-                def iterate(self, es_query, replica, per_page):
-                    for i in range(4):
-                        yield {"bundle_fqid": "a%d.b" % i}
-
-            post_search = MockDSSMethod()
-
-            def get(self, url, params):
-                if "files" in url:
-                    payload = {}
-                else:
-                    payload = {"bundle": {"files": files}}
-                res = Response()
-                res.status_code = 200
-                res.raw = io.BytesIO(json.dumps(payload).encode())
-                return res
-
-        dcplib.etl.http = MockDSSClient()
         with tempfile.TemporaryDirectory() as td:
-            e = dcplib.etl.DSSExtractor(staging_directory=td,
-                                        content_type_patterns=["application/json"],
-                                        filename_patterns=["*.json"],
-                                        dss_client=MockDSSClient())
-            e.extract(transformer=tf,
-                      loader=ld,
-                      finalizer=fn,
-                      query={"test": True},
-                      max_workers=2,
-                      max_dispatchers=1)
-            self.assertEqual(calls["tf"], 4)
-            self.assertEqual(calls["ld"], 4)
-            self.assertEqual(calls["fn"], 1)
 
-            e = dcplib.etl.DSSExtractor(staging_directory=td,
-                                        content_type_patterns=["application/json"],
-                                        filename_patterns=["*.json"],
-                                        dss_client=MockDSSClient())
-            e.extract(transformer=tf,
-                      loader=ld,
-                      finalizer=fn,
-                      query={"test": True},
+            e = dcplib.etl.DSSExtractor(
+                staging_directory=td,
+                content_type_patterns=["application/json"],
+                filename_patterns=["*.json"],
+                dss_client=MockDSSClient()
+            )
+            e.extract(query={"test": True},
                       max_workers=2,
                       max_dispatchers=1,
-                      dispatch_executor_class=concurrent.futures.ProcessPoolExecutor)
-            self.assertEqual(calls["tf"], 4)
-            self.assertEqual(calls["ld"], 4)
-            self.assertEqual(calls["fn"], 2)
+                      transformer=tf,
+                      loader=ld,
+                      finalizer=fn)
+        self.assertEqual(calls["tf"], 4)
+        self.assertEqual(calls["ld"], 4)
+        self.assertEqual(calls["fn"], 1)
 
-            with self.assertRaises(TestETLException):
-                e.extract(transformer=tf,
-                          loader=ld,
-                          finalizer=error_fn,
-                          query={"test": True},
-                          max_workers=2,
-                          max_dispatchers=1)
-
+#    @unittest.skipIf(sys.version_info < (3, 6), "Only testing under Python 3.6+")
+    @unittest.skip("TODO: (akislyuk) fix serialization issues in DSSClient")
+    def test_etl_with_dispatcher(self):
+        import dcplib.etl
         with tempfile.TemporaryDirectory() as td:
-            files = []
-            for dispatch_on_empty_bundles in True, False:
-                e = dcplib.etl.DSSExtractor(staging_directory=td,
-                                            content_type_patterns=["application/json"],
-                                            filename_patterns=["*.json"],
-                                            dss_client=MockDSSClient(),
-                                            dispatch_on_empty_bundles=dispatch_on_empty_bundles)
 
-                e.extract(transformer=tf, loader=ld, finalizer=fn, query={"test": True}, max_workers=2)
-            self.assertEqual(calls["tf"], 12)
-            self.assertEqual(calls["ld"], 12)
-            self.assertEqual(calls["fn"], 4)
+            e = dcplib.etl.DSSExtractor(staging_directory=td,
+                                        content_type_patterns=["application/json"],
+                                        filename_patterns=["*.json"],
+                                        dss_client=MockDSSClient()
+                                        )
+            e.extract(
+                query={"test": True},
+                max_workers=2,
+                max_dispatchers=1,
+                dispatch_executor_class=concurrent.futures.ProcessPoolExecutor,
+                transformer=tf,
+                loader=ld,
+                finalizer=fn
+            )
+        self.assertEqual(calls["tf"], 0)
+        self.assertEqual(calls["ld"], 4)
+        self.assertEqual(calls["fn"], 1)
+
+    @unittest.skipIf(sys.version_info < (3, 6), "Only testing under Python 3.6+")
+    def new_etl_raises_error_correctly(self):
+        import dcplib.etl
+        with tempfile.TemporaryDirectory() as td:
+
+            e = dcplib.etl.DSSExtractor(staging_directory=td,
+                                        content_type_patterns=["application/json"],
+                                        filename_patterns=["*.json"],
+                                        dss_client=MockDSSClient()
+                                        )
+            with self.assertRaises(TestETLException):
+                e.extract(query={"test": True},
+                          max_workers=2,
+                          max_dispatchers=1,
+                          transformer=tf,
+                          loader=ld,
+                          finalizer=error_fn
+                          )
+
+    @unittest.skipIf(sys.version_info < (3, 6), "Only testing under Python 3.6+")
+    def test_etl_handles_dispatch_on_empty_bundles_is_true(self):
+        import dcplib.etl
+        with tempfile.TemporaryDirectory() as td:
+
+            e = dcplib.etl.DSSExtractor(staging_directory=td,
+                                        content_type_patterns=["application/json"],
+                                        filename_patterns=["*.json"],
+                                        dss_client=MockDSSClient(),
+                                        dispatch_on_empty_bundles=True
+                                        )
+
+            e.extract(query={"test": True}, max_workers=2, transformer=tf, loader=ld, finalizer=fn)
+        self.assertEqual(calls["tf"], 4)
+        self.assertEqual(calls["ld"], 4)
+        self.assertEqual(calls["fn"], 1)
+
 
 if __name__ == '__main__':
     unittest.main()
