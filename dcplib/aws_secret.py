cbbfc4aec5c524441e33a0c8e34e66703ec96b53
Sam Pierson <sam@sampierson.com>
2019-07-08 21:43:50 -0300

    AwsSecret: add debug prints

--- dcplib/aws_secret.py
+++ dcplib/aws_secret.py
@@ -17,7 +17,10 @@ class AwsSecret:
     Update handles create vs update and undeletion if necessary.
     """
 
+    debug_logging = False
+
     def __init__(self, name):
+        self._debug("AwsSecret.__init__({})".format(name))
         self.name = name
         self.secrets_mgr = boto3.client(service_name='secretsmanager')
         self.secret_metadata = None
@@ -46,11 +49,14 @@ class AwsSecret:
 
     def update(self, value):
         if not self.exists_in_aws:
+            self._debug("AwsSecret.update({}) creating...".format(self.name))
             self.secrets_mgr.create_secret(Name=self.name, SecretString=value)
             self._load()
         else:
             if self.is_deleted:
+                self._debug("AwsSecret.update({}) restoring...".format(self.name))
                 self._restore()
+            self._debug("AwsSecret.update({}) updating...".format(self.name))
             self.secrets_mgr.put_secret_value(SecretId=self.arn, SecretString=value)
 
     def delete(self):
@@ -64,11 +70,19 @@ class AwsSecret:
         try:
             response = self.secrets_mgr.describe_secret(SecretId=self.name)
             if response:
+                self._debug("AwsSecret.load({}) it exists".format(self.name))
                 self.secret_metadata = response
         except ClientError as e:
-            if e.response['Error']['Code'] != 'ResourceNotFoundException':
+            if e.response['Error']['Code'] == 'ResourceNotFoundException':
+                #  Normal operation, secret does not exist yet.
+                self._debug("AwsSecret.load({}) ResourceNotFoundException".format(self.name))
+            else:
                 raise e
 
     def _restore(self):
         self.secrets_mgr.restore_secret(SecretId=self.arn)
         self._load()
+
+    def _debug(self, message):
+        if self.debug_logging:
+            print(message)
