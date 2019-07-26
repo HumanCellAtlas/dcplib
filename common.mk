596c7c951eef63835db8554faa5331aa232b9b54
Andrey Kislyuk <kislyuk@gmail.com>
2019-07-26 09:31:42 -0700

    common.mk: Remove old build products before releasing

--- common.mk
+++ common.mk
@@ -17,7 +17,7 @@ release_patch:
 release:
 	@if [[ -z $$TAG ]]; then echo "Use release_{major,minor,patch}"; exit 1; fi
 	git pull
-	git clean -x --force $$(python setup.py --name)
+	git clean -x --force $$(python setup.py --name) dist
 	sed -i -e "s/version=\([\'\"]\)[0-9]*\.[0-9]*\.[0-9]*/version=\1$${TAG:1}/" setup.py
 	git add setup.py
 	TAG_MSG=$$(mktemp); \
